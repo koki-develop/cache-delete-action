10fdadcf7c74293ae35ee43a0df651e610d26994
koki-develop <kou.pg.0131@gmail.com>
2024-08-30 22:34:28 +0900

    Implement action

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,42 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 export const main = async () => {
   try {
     const inputs = {
-      message: core.getInput("message"),
+      key: core.getInput("key", { required: true }),
+      ref: core.getInput("ref") || undefined,
+      token: core.getInput("token"),
+      failOnNotFound: core.getInput("fail-on-not-found") === "true",
     } as const;
 
-    core.info(inputs.message);
-    core.setOutput("message", inputs.message);
+    const octokit = github.getOctokit(inputs.token);
+
+    // check if cache exists
+    const { data } = await octokit.rest.actions.getActionsCacheList({
+      per_page: 1,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      key: inputs.key,
+      ref: inputs.ref,
+    });
+    if (data.actions_caches.length === 0) {
+      if (inputs.failOnNotFound) {
+        throw new Error("Cache not found.");
+      }
+      core.info("Cache not found.");
+      return;
+    }
+
+    // delete cache
+    core.info("Deleting cache...");
+    await octokit.rest.actions.deleteActionsCacheByKey({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      key: inputs.key,
+      ref: inputs.ref,
+    });
+    core.info("Cache deleted.");
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message);
